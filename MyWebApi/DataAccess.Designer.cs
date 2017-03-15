ecc9185b1df7fb31ea890795251efa15a626ab20
M1036083 <M1036083@A2MD19414>
2017-03-15 11:49:06 +0530

    code change

--- MyWebApi/DataAccess.Designer.cs
+++ MyWebApi/DataAccess.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\M1036083\Documents\Visual Studio 2015\Projects\EmployeeProjectwithJenkins\MyWebApi\DataAccess.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\M1036083\Documents\Visual Studio 2015\Projects\TeamCityProject\MyWebApi\DataAccess.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
